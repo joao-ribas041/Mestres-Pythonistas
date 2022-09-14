c83068e5759f54b2c8c997c7cbe7797910d7e59b
JoaoRibas041 <ribas.joao1410@gmail.com>
2022-09-13 22:28:08 -0300

    app04 - Strings dinâmicas

---
+++ app04.py
@@ -0,0 +1,40 @@
+# Strings dinâmicos
+nome = 'João'
+email = 'joao@gmail.com'
+# Olá rafael, você cadastrou o email rafael@gmail.com, essa
+# informação está correta?
+print(f'Olá {nome}, você cadastrou o email {email}, essa informação está correta?')
+
+
+# Desafio 1
+
+# Crie os seguinte strings dinâmicos
+
+nome = 'Carol'
+
+hobby = 'ouvir Música'
+
+'Olá sou a Carol e gosto de ouvir musica'
+print(f'Olá sou a {nome} e gosto de {hobby}')
+
+# Desafio 2
+
+# Monte a seguintes palavras, usando as sílabas como base
+
+b = 'ba'
+
+parte2 = 'nica'
+
+a = 'a'
+
+r = 'ri'
+
+parte1 = 'eletrô'
+
+t = 'te'
+
+# Resultado
+
+'bateria eletrônica'
+
+print(f'{b}{t}{r}{a} {parte1}{parte2}')
