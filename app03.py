6118fabb08e1066b545236c950c446f0d17c50a4
JoaoRibas041 <ribas.joao1410@gmail.com>
2022-09-13 22:20:32 -0300

    app3

---
+++ app03.py
@@ -0,0 +1,20 @@
+print('Codar todos os dias!')
+print("Codar todos os dias!!!")
+print('''Estamos codando todos os dias
+E estou aprendendo muito!''')
+# Caracteres de escape
+print('Olá meu nome é \n João')
+print('Codas todos \'os dias')
+print('Arquivo localizado em \\c:drive\\arquivo1.txt')
+nome = 'Carol'
+# print(len(nome))
+
+# DESAFIO
+# Vamos codar!
+# Vamos 'codar!'
+# Vamos
+# 'codar'
+
+print('Vamos codar!')
+print('Vamos \'codar!\'')
+print('Vamos \n"codar"')
