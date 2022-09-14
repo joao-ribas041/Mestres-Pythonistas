393f429ff43eb9f80c34679b1642834f8f7f40e8
JoaoRibas041 <ribas.joao1410@gmail.com>
2022-09-13 22:40:54 -0300

    Métodos comuns de um string

---
+++ app05.py
@@ -0,0 +1,27 @@
+nome_curso = '  Edição de Vídeo   '
+print(nome_curso.upper())
+print(nome_curso.lower())
+print(nome_curso.strip())
+print(nome_curso.lstrip())
+print(nome_curso.rstrip())
+print(nome_curso.find('ção'))
+print(nome_curso.replace('Vídeo', 'Música'))
+print('https://sc.olx.com.br/?o=90&q=relogio'.replace('relogio', 'carro'))
+
+
+# DESAFIO 🥇
+
+# Através da criação de string dinâmicos e os métodos de um string que acabou de aprender, use como base as variáveis a seguir para criar as seguintes frases
+
+a = 'é'
+
+b = 'MELHOR'
+
+c = 'QUE'
+
+d = 'feito'
+
+e = 'perfeito'
+
+print('É melhor FEITO que PERFEITO')
+print(f'{a.upper()} {b.lower()} {d.upper()} {c.lower()} {e.upper()}')
