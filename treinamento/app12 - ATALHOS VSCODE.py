1f3d2715758e31dfa5310fff082b9739cb0e9eea
JoaoRibas041 <ribas.joao1410@gmail.com>
2022-09-15 23:36:07 -0300

    ATALHOS VSCODE

---
+++ treinamento/app12 - ATALHOS VSCODE.py
@@ -0,0 +1,40 @@
+# comentar e descomentar por linhas #
+# ctrl + k, ctrl + c (comenta)
+# ctrl + k, ctrl + u (descomenta)
+
+# comentar e descomentar blocos de codigos """
+# shift + alt + a
+
+# Mover linha do codigo
+# alt + seta(baixo ou cima)
+
+# duplicar linhas
+# alt + shift + seta(baixo ou cima)
+
+# exibir e esconder aba lateral
+# ctrl + b
+
+# exibir/ocultar terminal
+# ctrl + '
+
+# Buscar termo
+# ctrl + f
+# ctrl + h (replace para termo)
+# ctrl + alt + enter
+
+# alterar entre arquivos
+# ctrl + p
+# ctrl + enter (abre arquivo ao lado)
+# ctrl + (1 ou 2) altera entre digitação dos arquivos
+
+# f12 (sobre a classe que voce esta usando abre o codigo fonte dela)
+
+nome_curso = '  Edição de Vídeo   '
+print(nome_curso.upper())
+print(nome_curso.lower())
+print(nome_curso.strip())
+print(nome_curso.lstrip())
+print(nome_curso.rstrip())
+print(nome_curso.find('ção'))
+print(nome_curso.replace('Vídeo', 'Música'))
+print('https://sc.olx.com.br/?o=90&q=relogio'.replace('relogio', 'carro'))
