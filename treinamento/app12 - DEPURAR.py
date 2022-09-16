a3ca015058ffeafa22f8efef25042d0de21b358c
JoaoRibas041 <ribas.joao1410@gmail.com>
2022-09-16 20:47:52 -0300

    Depurar codigo VSCODE

---
+++ treinamento/app12 - DEPURAR.py
@@ -0,0 +1,15 @@
+print('Olá')
+
+
+def calcular_preco_combo(pizza, refrigerante):
+    total = pizza + refrigerante
+    print(total)
+
+
+calcular_preco_combo(30, 20)
+print('Programa finalizado')
+# F9 - Marcar breakpoint
+# F5 - abrir modo debug
+# f10 - Passar sobre uma linha, SEM entrar em metodo internos
+# F11 - Passar sobre uma linha e entrar em método interno
+# SHITF + F5 - Interromper debug
