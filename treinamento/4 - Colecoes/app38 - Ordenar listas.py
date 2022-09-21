9fd7765055c5e8e79fd8779867e2f36dd1896d0d
JoaoRibas041 <ribas.joao1410@gmail.com>
2022-09-21 20:43:30 -0300

    Listas

---
+++ treinamento/4 - Colecoes/app38 - Ordenar listas.py
@@ -0,0 +1,11 @@
+valores = [31, 23, 6, 36, 21, 33, 37, 7, 20, 23]
+
+# Ordenador em ordem crescente
+valores.sort()
+# Ordenar em ordem descrecente
+# valores.sort(reverse=True)
+print(valores)
+
+# Inverter uma lista
+valores.reverse()
+print(valores)
