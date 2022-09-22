e7c8df1e70f48bcbeb20e7103a42b54f0372d500
JoaoRibas041 <ribas.joao1410@gmail.com>
2022-09-21 22:16:33 -0300

    ENUMERATE(indice)

---
+++ treinamento/4 - Colecoes/app44 - Enumerate.py
@@ -0,0 +1,24 @@
+# Enumerate
+for indice, numero in enumerate(range(1, 11), 1):
+    print(indice, numero)
+    if indice == 5:
+        print('Estamos na metade da lista')
+print()
+nomes = ['nome1', 'nome2', 'nome3', 'nome4', 'nome5']
+
+for nome in enumerate(nomes, 1):
+    print(indice, nome)
+    if indice == 3:
+        print('Já temos 3 pessoas registradas')
+
+# Desafio1
+# Itere sobre a lista abaixo exibindo o número
+# do índice + nome da fruta. Porém quando o
+# índice for 3 exiba 'N°' índice + nome da fruta EM PROMOÇÃO
+
+frutas = ['Maça', 'Laranja', 'Morango', 'Limão']
+
+for indice, fruta in enumerate(frutas, 0):
+    print(f'{indice} {fruta}.')
+    if indice == 3:
+        print(f'{indice}, {fruta} EM PROMOÇÃO')
