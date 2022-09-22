de08306dd9eaa895855e0b59c922518f9637312a
JoaoRibas041 <ribas.joao1410@gmail.com>
2022-09-21 22:17:14 -0300

    ENUMERATE(índice)

--- treinamento/4 - Colecoes/app44 - Enumerate.py
+++ treinamento/4 - Colecoes/app44 - Enumerate.py
@@ -19,6 +19,7 @@ for nome in enumerate(nomes, 1):
 frutas = ['Maça', 'Laranja', 'Morango', 'Limão']
 
 for indice, fruta in enumerate(frutas, 0):
-    print(f'{indice} {fruta}.')
     if indice == 3:
         print(f'{indice}, {fruta} EM PROMOÇÃO')
+    else:
+        print(f'{indice} {fruta}.')
