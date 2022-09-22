38321d0c7476238a574319f840c81d3f83686754
JoaoRibas041 <ribas.joao1410@gmail.com>
2022-09-21 21:54:29 -0300

    Tuplas

---
+++ treinamento/4 - Colecoes/app41 - Tuplas.py
@@ -0,0 +1,20 @@
+# Tuplas
+site1 = 'youtube.com'
+site2 = 'facebook.com'
+site3 = 'instagram.com'
+# ou
+valor1 = 23
+valor2 = 43
+valor3 = 65
+sites = ('youtube.com', 'facebook.com', 'instagram.com', 54, True)
+valores = (23, 43, 65, False)
+print(valores[2])
+
+# União de tuplas
+dados_do_sistema = sites + valores
+print(dados_do_sistema)
+
+# Acesso de valores em uma tupla
+print(dados_do_sistema[2])
+print(sites)
+print(valores)
