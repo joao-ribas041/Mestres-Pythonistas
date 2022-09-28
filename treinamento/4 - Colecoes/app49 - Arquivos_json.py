ee1ea8af7099bc6f22da41a0511df81f1d3a447b
João Ribas <ribas.joao1410@gmail.com>
2022-09-27 21:07:09 -0300

    Resolvido desafio JSON

--- treinamento/4 - Colecoes/app49 - Arquivos_json.py
+++ treinamento/4 - Colecoes/app49 - Arquivos_json.py
@@ -2,7 +2,7 @@
 from importlib.resources import path
 import json
 from pathlib import Path
-
+'''
 # carros = [
 #     {"Marca": "Nissan", "Preço": 45.000, "Cor": "Azul"},
 #     {"Marca": "Ford", "Preço": 75.000, "Cor": "Verde"},
@@ -16,7 +16,7 @@ arquivo_carro_json = Path('carros.json').read_text()
 arquivo_json = json.loads(arquivo_carro_json)
 print(arquivo_json[0]['Marca'] + ' ' + str(arquivo_json[0]['Preço']))
 print(arquivo_json[1]['Marca'] + ' ' + str(arquivo_json[1]['Preço']))
-
+'''
 
 # Seu único desafio é de encontrar e exibir na tela a 'ability' 'lightning-rod'
 # Esse é o único desafio
@@ -24,6 +24,8 @@ print(arquivo_json[1]['Marca'] + ' ' + str(arquivo_json[1]['Preço']))
 # Pode parecer simples, mas se ainda não se acostumou em navegar em dicionários, pode
 # levar um tempo então vá com calma e depois volte aqui para ver o resultado
 
+print('\n')
 arquivo_pikachu = Path('pikachu.json').read_text()
 a_json = json.loads(arquivo_pikachu)
-print(a_json[1]['ability'])
+print((a_json['abilities'][1]['ability']['name']))
+
