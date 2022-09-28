fb023d895146da7c2e26be328568c87d80b8ecfa
João Ribas <ribas.joao1410@gmail.com>
2022-09-27 22:31:02 -0300

    Create app50 - Conversao.py

---
+++ treinamento/4 - Colecoes/app50 - Conversao.py
@@ -0,0 +1,21 @@
+""" idade = input('Digite sua idade ')
+print(int(idade) > 17)
+
+
+ano_publicacao = 2020
+print('Este livro foi criado em' + str(ano_publicacao))
+
+altura = input('Altura da parede ')
+print(float(altura) > 2.5)
+ """
+# Conversões entre coleções
+saudacao = 'Hello!'
+print(list(saudacao))
+print(set(saudacao))
+print(tuple(saudacao))
+print(list(range(30)))
+
+numeros = [10,20,20,50]
+print(numeros)
+print(set(numeros))
+print(tuple(numeros))
