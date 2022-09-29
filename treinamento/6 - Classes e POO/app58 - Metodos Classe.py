2f35f654c43844513e803398ff080e75a7eee8e0
João Ribas <ribas.joao1410@gmail.com>
2022-09-28 22:12:47 -0300

    Métodos de classes

---
+++ treinamento/6 - Classes e POO/app58 - Metodos Classe.py
@@ -0,0 +1,23 @@
+# Metodos de uma classe -> ligar, desligar, exibir dados do computador
+class Computador:
+    def __init__(self, marca, memoria_ram,placa_de_video):
+        self.marca = marca
+        self.memoria_ram = memoria_ram
+        self.placa_de_video = placa_de_video
+
+    def ligar(self):
+        print('Estou ligando o computador')
+    def desligar(self):
+        print('Estou desligando')
+    def exibir_dados_do_computador(self):
+        print(f'Computador da marca {self.marca}, com {self.memoria_ram} de memória ram e que usa a placa de vídeo {self.placa_de_video}')
+    
+computador1 = Computador('Avell','32gb','1660ti')
+computador1.ligar()
+computador1.desligar()
+computador1.exibir_dados_do_computador()
+
+computador2 = Computador('Asus', '8gb', 'Intel')
+computador1.ligar()
+computador1.desligar()
+computador1.exibir_dados_do_computador()
