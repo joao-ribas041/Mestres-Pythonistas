095df46447fe938b120ec9e52589143b08d505c0
João Ribas <ribas.joao1410@gmail.com>
2022-10-03 21:12:44 -0300

    if main

---
+++ treinamento/7 - Modulos/app71 - if main.py/carro.py
@@ -0,0 +1,6 @@
+def ligar_carro():
+    print('Ligando o carro.')
+
+if __name__ == '__main__':
+    print('Tirando carro da garagem')
+    print(f'Estamos no arquivo {__name__}')
---
+++ treinamento/7 - Modulos/app71 - if main.py/moto.py
@@ -0,0 +1,6 @@
+def ligar_moto():
+    print('Ligando a moto.')
+
+if __name__ == '__main__':
+    print('Tirando moto da garagem')
+    print(f'Estamos no arquivo {__name__}')
