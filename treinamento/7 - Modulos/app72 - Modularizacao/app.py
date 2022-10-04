84099159e248fc3eec47998e2f6f04e58eba0596
João Ribas <ribas.joao1410@gmail.com>
2022-10-03 21:19:04 -0300

    Modularização no Python

---
+++ treinamento/7 - Modulos/app72 - Modularizacao/app.py
@@ -0,0 +1,7 @@
+from banco_de_dados import buscar_usuario, iniciar_banco_de_dados
+from configuracoes import senhas, usuarios_maximos
+from processamento_imagem import Camera
+
+#
+#
+#
---
+++ treinamento/7 - Modulos/app72 - Modularizacao/banco_de_dados.py
@@ -0,0 +1,6 @@
+def iniciar_banco_de_dados():
+    print('Iniciando conexao')
+    print('Criando tabelas iniciais')
+
+def buscar_usuario():
+    print('Buscando usuários')
