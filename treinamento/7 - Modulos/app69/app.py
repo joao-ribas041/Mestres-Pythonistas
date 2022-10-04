119f01e32d9d9658473f32bea34ae3b7623449d7
João Ribas <ribas.joao1410@gmail.com>
2022-10-03 21:00:31 -0300

    Quando usar módulos

---
+++ treinamento/7 - Modulos/app69/app.py
@@ -0,0 +1,10 @@
+# Quando usar módulos
+# 1° - Separar funcionalidades relacionadas
+# 2° - Não acoplar o seu código
+# 3° - Não ter um arquivo gigante
+
+from banco_de_dados import buscar_usuario
+from configuracoes import senha
+# from xxxx import a,b,c,d
+
+buscar_usuario()
---
+++ treinamento/7 - Modulos/app69/configuracoes.py
@@ -0,0 +1,7 @@
+senha = '1234ASD!'
+email_pessoal = 'jhonatan@gmail.com'
+conexoes_maximas = 5
+
+def obter_senha():
+    senha = 'F!R#RD'
+    return senha
