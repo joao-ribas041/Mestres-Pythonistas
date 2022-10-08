b62048915d87dd2e9b8378ba30cc0b7874ff96cb
João Ribas <ribas.joao1410@gmail.com>
2022-10-07 22:37:20 -0300

    Travei

--- treinamento/8 - APIs/app78.py
+++ treinamento/8 - APIs/app78.py
@@ -1,4 +1,42 @@
+from msilib import init_database
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
+# Criar uma API flask
+app = Flask(__name__)
 
+
+# Criar uma instância de SQLAlchemy
+app.config['SECRET_KEY'] = 'FSD24323f$@!s@'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+
+db = SQLAlchemy(app)
+db:SQLAlchemy
+
+# Definir a estrutura da tabela Postagem
+# id_postagem, titulo, autor
+class Postagem(db.Model):
+    __tablename__ = 'postagem'
+    id_postagem = db.Column(db.Integer, primary_key=True)
+    titulo = db.Column(db.String)
+    id_autor = db.Column(db.Integer, db.ForeignKey('autor.id_autor'))
+
+# Definir a estrutura da tabela Autor
+# id_autor, nome, email, senha, admin, postagens
+class Autor(db.Model):
+    __tablename__ = 'autor'
+    id_autor = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String)
+    email = db.Column(db.String)
+    senha = db.Column(db.String)
+    admin = db.Column(db.Boolean)
+    postagens = db.relationship('postagem')
+    
+# Executar o comando para criar o banco de dados
+#db.drop_all()
+db.create_all()
+
+# Criar usuários administradores
+#autor = Autor(nome='Joao',email='joao@gmail.com',senha='123456',admin=True)
+#db.session.add(autor)
+#db.session.commit() """
