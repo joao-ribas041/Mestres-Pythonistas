987b1f5d315a8395ba01bbada966f8de50b6e532
João Ribas <ribas.joao1410@gmail.com>
2022-10-12 23:49:44 -0300

    APIs com autenticacao usando Requests

--- treinamento/8 - APIs/app80/app.py
+++ treinamento/8 - APIs/app80/app.py
@@ -4,6 +4,8 @@ resultado = requests.get('http://localhost:5000/login',auth=('Joao','123'))
 
 print(resultado.json())
 
-resultado_autores = requests.get('http://localhost:5000/autores',headers={'x-access-token': resultado.json()['token']})
+resultado_autores = requests.get('http://localhost:5000/autores',headers={'x-acess-token': resultado.json()['token']})
 
 print(resultado_autores.json())
+
+
