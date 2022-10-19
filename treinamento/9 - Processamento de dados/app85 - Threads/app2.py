89fe35600045963d8c8b3fc81e576001bf23bfe5
João Ribas <ribas.joao1410@gmail.com>
2022-10-18 23:46:00 -0300

    Threads 2

--- treinamento/9 - Processamento de dados/app85 - Threads/app2.py
+++ treinamento/9 - Processamento de dados/app85 - Threads/app2.py
@@ -1,17 +1,18 @@
 import webbrowser
 import threading
 import time
+import random
 
-def comentar(site):
-    if site == 8:
-        raise Exception(f'OUVE UM ERRO GRAVE {site}')
+def comentar(site, comentario):
     print(f'Entrando no site: {site}')
+    print(f'Deixando um comentario: {comentario}')
     time.sleep(5)
     print(f'Dados processados no site: {site}')
     
+comentarios = ['oi', 'ola', 'gostei', 'curti', 'muito bom']
 threads = []
-for site in range(100):
-    nova_thread = threading.Thread(target=comentar, args=(site,))
+for site in range(6):
+    nova_thread = threading.Thread(target=comentar, args=(site, random.choice(comentarios)))
     threads.append(nova_thread)
     
 for thread in threads:
