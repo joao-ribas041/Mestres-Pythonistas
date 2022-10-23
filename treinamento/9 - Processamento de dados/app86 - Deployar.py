b4c497c14ac410bf938a35252ae21d011febfcc9
João Ribas <ribas.joao1410@gmail.com>
2022-10-22 23:57:26 -0300

    app do Instalador

--- treinamento/9 - Processamento de dados/app86 - Deployar.py
+++ treinamento/9 - Processamento de dados/app86 - Deployar.py
@@ -1,5 +1,11 @@
 import time
 
+cont = 0
 while True:
     print('estamos funcionando :)')
     time.sleep(3)
+    cont += 1
+    if cont >=10:
+        print('Encerrando')
+        break
+print('finalizado')
