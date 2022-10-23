a917de51e7480c0497b8c97e05eb1c9c36bd6286
João Ribas <ribas.joao1410@gmail.com>
2022-10-23 14:09:43 -0300

    Update app86 - Deployar.py

--- treinamento/9 - Processamento de dados/app86 - Deployar.py
+++ treinamento/9 - Processamento de dados/app86 - Deployar.py
@@ -1,11 +1,6 @@
 import time
 
-cont = 0
 while True:
     print('estamos funcionando :)')
     time.sleep(3)
-    cont += 1
-    if cont >=10:
-        print('Encerrando')
-        break
-print('finalizado')
+    
